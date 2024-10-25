4b6dd7fe96b3bfbd0e17947fc4f5460a3d188077
sarrabousnina <sarra.bousnina@esprit.tn>
2024-10-25 11:40:36 +0100

    more changes

--- src/main/java/tn/esprit/sarra_bousnina_4se4/controllers/PisteRestConroller.java
+++ src/main/java/tn/esprit/sarra_bousnina_4se4/controllers/PisteRestConroller.java
@@ -1,12 +1,12 @@
 package tn.esprit.sarra_bousnina_4se4.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tn.esprit.sarra_bousnina_4se4.Entities.Piste;
 import tn.esprit.sarra_bousnina_4se4.services.PisteServicesImpl;
+
+import java.util.List;
+
 @AllArgsConstructor
 @RequestMapping("piste")
 @RestController
@@ -14,4 +14,18 @@ public class PisteRestConroller {
     private PisteServicesImpl pisteServices;
     @PostMapping("/addP")
     public Piste addPiste(@RequestBody Piste piste) {return pisteServices.addPiste(piste);}
+    @GetMapping("get/{numPiste}")
+    public Piste getPiste (@PathVariable Long numPiste )
+    {
+        return pisteServices.retreivePiste(numPiste);
+    }
+    @GetMapping("/getAll")
+    public List<Piste> getAll(){return pisteServices.retreiveAll();}
+    @PutMapping ("/update")
+    public Piste updatePiste(@RequestBody Piste piste) {return pisteServices.updatePiste(piste);}
+    @DeleteMapping("/delete/{numPiste}")
+    public void delete(@PathVariable Long numPiste){
+        pisteServices.deletePiste(numPiste);
+    }
+
 }
